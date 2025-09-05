b5ceb70604637f542451e756be077073aaed6dc6
Hritujeet <132835698+Hritujeet@users.noreply.github.com>
2025-09-05 16:25:36 +0530

    edits

--- src/app/api/projects/fetchprojects/route.ts
+++ src/app/api/projects/fetchprojects/route.ts
@@ -2,6 +2,10 @@ import {NextResponse} from "next/server";
 import {prisma} from "../../../../../utils/db";
 
 export async function GET() {
-    const projects = await prisma.project.findMany({});
+    const projects = await prisma.project.findMany({
+        orderBy: {
+            createdAt: "desc"
+        }
+    });
     return NextResponse.json({msg: "This is get projects", projects})
 }
