0a137dda80c25c5dd2ed2dbd1270275a10781291
Hritujeet <132835698+Hritujeet@users.noreply.github.com>
2025-09-26 16:29:44 +0530

    sitemap

---
+++ next-sitemap.config.ts
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: "https://hritujeet-portfolio.vercel.app", // Change this
+    generateRobotsTxt: true, // Generate robots.txt file
+    sitemapSize: 7000, // Optional: split sitemap if too big
+  };
+  
