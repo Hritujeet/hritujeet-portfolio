1c915d744e4d529246ccf97233441e7b420e844b
Hritujeet <132835698+Hritujeet@users.noreply.github.com>
2025-09-26 17:12:44 +0530

    config sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,5 +3,6 @@ module.exports = {
     siteUrl: "https://hritujeet-portfolio.vercel.app", // Change this
     generateRobotsTxt: true, // Generate robots.txt file
     sitemapSize: 7000, // Optional: split sitemap if too big
+    exclude: ["/dashboard/**"],
   };
   
